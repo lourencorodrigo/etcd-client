93af79f1912a9b3bd264544e0dc8b0ea09951a52
Rodrigo Lourenço <rsilvape@gmail.com>
2019-11-15 12:03:47 -0300

    Registering service worker

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Grid, Paper } from '@material-ui/core'
 import ConnectServer from '../../containers/ConnectServer'
 import { useStyles } from './styles'
-import TreeViewServer from '../../containers/TreeViewServer'
 
 const Home = () => {
   const classes = useStyles()
@@ -10,12 +9,7 @@ const Home = () => {
   return (
     <Grid container spacing={2}>
       <ConnectServer />
-      <Grid item xs={12} sm={4}>
-        <Paper className={classes.paper}>
-          <TreeViewServer />
-        </Paper>
-      </Grid>
-      <Grid item xs={12} sm={8}>
+      <Grid item xs={12} sm={12}>
         <Paper className={classes.paper}>xs=12 sm=6</Paper>
       </Grid>
     </Grid>
